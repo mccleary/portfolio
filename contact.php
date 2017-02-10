e5871f91bdc7227639c829eace3d150bc5a6dee4
trista <trista_mccleary@yahoo.com>
2017-02-09 21:31:42 -0500

    update contact form

--- contact.php
+++ contact.php
@@ -1,39 +1,81 @@
 <?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$human = intval($_POST['human']);
-		$from = 'Contact Form';
-		$to = 'trista_mccleary@yahoo.com';
-		$subject = 'Message from Contact form';
 
-		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
-		//Check if simple anti-bot test is correct
-		if ($human !== 5) {
-			$errHuman = 'Your anti-spam is incorrect';
-		}
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
-	}
+$errors = '';
+/* your email here */
+$myemail = 'trista_mccleary@yahoo.com';
+if(empty($_POST['name']) ||
+   empty($_POST['email']) ||
+   empty($_POST['phone']) ||
+   empty($_POST['message']))
+{
+	$errors .= "\n Error: all fields are required";
 }
-	}
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address)) {
+	$errors .= "\n Error: This is invalid email address";
+}
+
+if( empty($errors) ) {
+	$to = $myemail;
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new massage. ".
+	" Here are the address:\n Name: $name \n Email: $email_address \n Phone: $phone \n Message: $message";
+
+	$headers = "From: $myemail\n";
+	$headers .= "Reply-To: $email_address";
+
+	mail($to,$email_subject,$email_body,$headers);
+	// redirect to the 'thank you' page
+	header('Location: thank-you.html');
+}
 ?>
+<!Doctype html>
+<html lang="en">
+<head>
+
+	<title>Thank you!</title>
+
+	<link rel="icon" href="images/favicon.png">
+
+<!-- define some style elements-->
+
+<style>
+h1 {
+	font-family : 'Oxygen', Helvetica, sans-serif;
+	font-size: 26px;
+    letter-spacing: 2px;
+    line-height: 42px;
+    margin-bottom: 20px;
+    font-weight: bold;
+}
+
+label,a,body {
+	font-family : 'Oxygen', Helvetica, sans-serif;
+	font-size : 14px;
+}
+.respond {
+	padding: 100px 0;
+	text-align: center;
+}
+</style>
+
+<!-- a helper script for vaidating the form-->
+</head>
+<body>
+
+<div class="respond">
+	<h1>Thank you!</h1>
+	<p>Thank you for submitting the form. We will contact you soon!</p>
+</div>
+
+</body>
+</html>
